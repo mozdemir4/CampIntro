51e8c730a7ffe92565fb39d194e1c3b1f82c675a
mozdemir4 <muhammt.ozdemir41@gmail.com>
2021-10-15 00:23:25 +0300

    Add project files.

---
+++ CampIntro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Name = "Apple";
+            product1.Price = 25;
+            product1.StockQuantity = 5;
+            product1.Comments = "Very sweet an apple";
+
+
+            Product product2 = new Product();
+            product2.Name = "Lemon";
+            product2.Price = 10;
+            product2.StockQuantity = 5;
+            product2.Comments = "Yummy Lemon";
+
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Add(product1);
+            sepetManager.Add(product2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CampIntro/SepetManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampIntro
+{
+    class SepetManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Product added :"+product.Name);
+        }
+    }
+}
