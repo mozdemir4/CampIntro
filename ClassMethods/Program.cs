d97ac04615b1954945c892479d4d361028782c21
mozdemir4 <muhammt.ozdemir41@gmail.com>
2021-10-16 13:08:15 +0300

    update test

--- ClassMethods/Program.cs
+++ ClassMethods/Program.cs
@@ -7,7 +7,7 @@ namespace ClassMethods
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("asdsad");
+           
         }
     }
 }
