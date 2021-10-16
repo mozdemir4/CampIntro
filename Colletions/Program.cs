6cd2fe31aac3187fe0dd8ae7c7180ded195da334
mozdemir4 <muhammt.ozdemir41@gmail.com>
2021-10-17 00:24:38 +0300

    OOP Intro

---
+++ Colletions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Colletions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] { "Elif", "Fatma", "Muhammet", "Semiha" };
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[3]);
+
+            List<string> names2 = new List<string> { "Elif","Fatma","Caner","Kerem"};
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+            names2.Add("Ahmet");
+            Console.WriteLine(names2[4]);
+            Console.WriteLine(names2[0]);
+
+            
+
+
+
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public static void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName +" "+"added.");
+        }
+        public static void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName +" "+"updated.");
+        }
+
+     
+    }
+}
