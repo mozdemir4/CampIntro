dc6597c4259a8c50bf7dd46acef372b1bed6fc5a
mozdemir4 <muhammt.ozdemir41@gmail.com>
2021-10-18 14:38:31 +0300

    Constructors

---
+++ Constructors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id=1,FirstName="Muhammet",LastName="ÖZDEMİR",City="KOCAELİ"};
+
+            Customer customer2 = new Customer(2, "Arzu", "CAN", "MANİSA");
+            
+        }
+    }
+    class Customer
+    {
+
+        //default constructor // new işlemini yaptığımızda başlangıçta ilgili bloğu yazmasak
+        // bile arka planda böyle bir constructor oluşur
+        public Customer()
+        {
+
+        }
+        public Customer(int id,string firstName,string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+            Console.WriteLine("Parametleri yapıcı blog çalıştı."+" "+ FirstName);
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
